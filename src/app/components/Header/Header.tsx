2d09cbfcef3ef0ace4d2293225a6ba2f560c589e
gemperline <adamgemperline@gmail.com>
2024-08-24 16:29:31 -0700

    made changes to navbar and layout

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,11 +1,56 @@
+import { Grid } from '@mui/material';
 import React from 'react';
+import { makeStyles } from 'tss-react/mui';
+
+const useStyles = makeStyles()({
+    root: {
+      width: '100%',
+      padding: '12px 6px',
+      display: 'flex',
+      alignItems: 'center',
+      backgroundColor: 'red',
+    },
+    subRoot: {
+      width: '100%',
+    },
+    adbanner: {
+        justifyContent: 'center',
+        display: 'flex',
+    },
+    rootWarning: {
+      backgroundColor: '#FEF6EE',
+      border: '1px solid #FFA22E',
+      justifyContent:'left',
+      '& #custom-banner-text': {
+        color: '#663B01',
+      },
+    },
+    rootError: {
+      backgroundColor: '#ffd9cc',
+      '& #custom-banner-text': {
+        color: '#611a16',
+      },
+    },
+    icon: {
+      marginLeft: '6px',
+      marginRight: '12px',
+    },
+    text: {
+      fontSize: 14,
+      alignSelf: 'center',
+    },
+    action: {
+      marginLeft: '12px',
+    },
+});
 
 export const Header = (props) => {
+    const { classes } = useStyles();
     return (
-        <>
+        <Grid className={classes.root}>
             <div style={{ height: props?.height }}>
                 {'This is the header'}
             </div>
-        </>
+        </Grid>
     );
 }
--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -190,74 +190,71 @@ export const Navbar = () => {
     };
     
     return (
-        <>
-            <ClickAwayListener onClickAway={closeMenu}>
-                <Paper classes={{ root: `${classes.navMenu} ${open ? classes.drawer : classes.drawerCollapse}` }}>
-                    <Grid className={classes.navBar}>
-                        <Grid className={classes.leftMenu}>
-                            <IconButton onClick={toggleMenuOpen}>
-                                {open ? <CloseIcon className={classes.navIcon} /> : <MenuIcon className={classes.navIcon} />}
-                            </IconButton>
-                        </Grid>
-                        <Grid className={classes.companyName}>
-                            Modern Magic
-                        </Grid>
-                        <Grid className={classes.rightMenu}>
-                            <IconButton>
-                                <AccountCircleIcon className={classes.navIcon} />
-                            </IconButton>
-                            <IconButton>
-                                <ShoppingCartIcon className={ classes.navIcon } />
-                            </IconButton>
-                        </Grid>
-                    </Grid>
-                    {open && (
-                        <div
-                        style={{ overflow: 'hidden' }}
-                        className={cx({
-                          [classes.listWrapperExpanded]: navigationExpanded,
-                          [classes.listWrapperCollapsed]: !navigationExpanded,
-                        })}
-                      >
-                        <List
-                          id="global-nav-list"
-                          disablePadding
-                          className={cx(classes.menuList, {
-                            [classes.menuListExpanded]: open,
-                            [classes.menuListCollapsed]: !open,
-                          })}
-                          sx={{ overflowY: 'auto' }}
-                        //   onMouseEnter={() => {
-                        //     if (!navigationExpanded) {
-                        //       toggleMenu();
-                        //       setNavigationExpandedByHover(true);
-                        //     }
-                        //   }}
-                        //   onMouseLeave={() => {
-                        //     if (navigationExpandedByHover) {
-                        //       setNavigationExpandedByHover(false);
-                        //       closeMenu();
-                        //     }
-                        //   }}
-                        >
-                          {navigationMenu.map((menuItem, index) => (
-                            <PrivateComponent key={index}>
-                              <NavMenuItem
-                                menuItem={menuItem}
-                                index={index}
-                                open={open}
-                                navigationExpanded={navigationExpanded}
-                                menuItemClicked={menuItemClicked}
-                              />
-                            </PrivateComponent>
-                          ))}
-                        </List>
-                      </div>
-                    )}
-                </Paper>
-            </ClickAwayListener>
-            <Divider className={classes.divider} />
-        </>
+      <ClickAwayListener onClickAway={closeMenu}>
+          <Paper classes={{ root: `${classes.navMenu} ${open ? classes.drawer : classes.drawerCollapse}` }}>
+              <Grid className={classes.navBar}>
+                  <Grid className={classes.leftMenu}>
+                      <IconButton onClick={toggleMenuOpen}>
+                          {open ? <CloseIcon className={classes.navIcon} /> : <MenuIcon className={classes.navIcon} />}
+                      </IconButton>
+                  </Grid>
+                  <Grid className={classes.companyName}>
+                      Modern Magic
+                  </Grid>
+                  <Grid className={classes.rightMenu}>
+                      <IconButton>
+                          <AccountCircleIcon className={classes.navIcon} />
+                      </IconButton>
+                      <IconButton>
+                          <ShoppingCartIcon className={ classes.navIcon } />
+                      </IconButton>
+                  </Grid>
+              </Grid>
+              {open && (
+                  <div
+                  style={{ overflow: 'hidden' }}
+                  className={cx({
+                    [classes.listWrapperExpanded]: navigationExpanded,
+                    [classes.listWrapperCollapsed]: !navigationExpanded,
+                  })}
+                >
+                  <List
+                    id="global-nav-list"
+                    disablePadding
+                    className={cx(classes.menuList, {
+                      [classes.menuListExpanded]: open,
+                      [classes.menuListCollapsed]: !open,
+                    })}
+                    sx={{ overflowY: 'auto' }}
+                  //   onMouseEnter={() => {
+                  //     if (!navigationExpanded) {
+                  //       toggleMenu();
+                  //       setNavigationExpandedByHover(true);
+                  //     }
+                  //   }}
+                  //   onMouseLeave={() => {
+                  //     if (navigationExpandedByHover) {
+                  //       setNavigationExpandedByHover(false);
+                  //       closeMenu();
+                  //     }
+                  //   }}
+                  >
+                    {navigationMenu.map((menuItem, index) => (
+                      <PrivateComponent key={index}>
+                        <NavMenuItem
+                          menuItem={menuItem}
+                          index={index}
+                          open={open}
+                          navigationExpanded={navigationExpanded}
+                          menuItemClicked={menuItemClicked}
+                        />
+                      </PrivateComponent>
+                    ))}
+                  </List>
+                </div>
+              )}
+          </Paper>
+      </ClickAwayListener>
     )
 };
 
